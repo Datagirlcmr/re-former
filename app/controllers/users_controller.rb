0f3931c93f6b2e4809793bc9f85ca8e4ca4a9fbb
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-01-08 12:28:42 -0300

    Modify new user form to use form_for, Create user edit form using form_for; Show errors when try to create or edit invalid user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:username, :password, :email)
   end
